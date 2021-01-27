f06f3698e486eddc11c9954d64e615262884646d
fernandadsilva <fdsferna@gmail.com>
2021-01-26 21:38:25 -0300

    create repo on git

---
+++ Readme.md
@@ -0,0 +1,40 @@
+<h1 align="center">
+    <img alt="Happy" title="Happy" src="https://github.com/rocketseat-education/nlw-03-discovery/raw/main/.github/logo.svg" />
+</h1>
+
+<p align="center">
+  <a href="#rocket-Technologies">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+<p align="center">
+ <img src="https://img.shields.io/static/v1?label=PRs&message=welcome&color=8257E5&labelColor=000000" alt="PRs welcome!" />
+
+  <img alt="License" src="https://camo.githubusercontent.com/3fca0593a01fe764320a9ea2db6ec0fc306d1f569a69ab90addcc198ab6083d8/68747470733a2f2f696d672e736869656c64732e696f2f7374617469632f76313f6c6162656c3d6c6963656e7365266d6573736167653d4d495426636f6c6f723d313543334436266c6162656c436f6c6f723d303030303030">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Happy" src="https://github.com/rocketseat-education/nlw-03-discovery/raw/main/.github/happy.png" width="100%">
+</p>
+
+## 🚀 Technologies
+
+This project was developed with the technologies
+
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/pt-br/)
+- [SQLite](https://www.sqlite.org/index.html)
+- [Hbs](https://handlebarsjs.com/)
+
+## 💻 Project
+
+Happy is a platform for locating orphanages near your region and scheduling visits.
+
+
+
+
+---
+<sup>Project developed with the tutorship of [Rocketseat](rocketseat.com.br).</sup>
---
+++ src/database/fakedata.js
@@ -0,0 +1,31 @@
+module.exports=[
+  {
+    id: 1,
+    name:"Lar das meninas",
+    description:"Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+    images:[
+      "https://images.unsplash.com/photo-1605639964700-3128da13d7ac?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+        "https://images.unsplash.com/photo-1603162496424-afdc5011767a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+    ],
+    lat:"-23.295829",
+    lng:"-51.167745",
+    instruction: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hour: "Horário de visitas Das 18h até 8h",
+    opening_on_weekwnds: "1"
+  },
+  {
+    id: 2,
+    name:"Lar dos Meninos",
+    description:"Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+    images:[
+      "https://images.unsplash.com/photo-1605639964700-3128da13d7ac?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+        "https://images.unsplash.com/photo-1603162496424-afdc5011767a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+    ],
+    lat:"-23.2964565",
+    lng:"-51.1672037",
+    instruction: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hour: "Horário de visitas Das 8h até 18h",
+    opening_on_weekwnds: "0"
+
+  }
+]
